68b06c7f9a8631fb25fd6ca72b3295b433e1bd72
AndrewGx <128672267+DevAndrewGx@users.noreply.github.com>
2023-07-05 15:18:24 -0500

    First commit on this repository

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function modal() {
+    let contModal = document.getElementById("modal");
+    // le agreagamos la clase para que se muestre cuando le demos click al boton
+    contModal.classList.add("active");
+
+}
